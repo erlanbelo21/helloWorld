b4870825ebd57a0e85004ed922652403ab4ac58a
erlanbelo21 <erlanbelo@hotmail.com>
2023-08-18 12:37:56 -0300

    mod2

--- src/app/acao/acao.page.ts
+++ src/app/acao/acao.page.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcaoPage implements OnInit {
 
+  listaProdutos = [
+    {nome: "Flash",
+    descricao: "Cor",
+    valor: 20
+    },
+
+    {nome: "Velozes e Furiosos",
+    descricao: "Cor: Preto",
+    valor: 35
+    },
+
+    {nome: "Besouro Azul",
+    descricao: "Cor: AZUL",
+    valor: 45
+    },
+
+    {nome: "Guardiões da Galaxia V.3",
+    descricao: "Cor: BRANCO",
+    valor: 500
+    },
+
+    {nome: "Megatubarçao 2",
+    descricao: "Cor: PRETO",
+    valor: 350
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
